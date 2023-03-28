ec78fb0f7b4e2dc01837c8fff9e1946b0d847556
zhangluwei <zhangluwei@yunzhangfang.com>
2023-03-28 11:11:17 +0800

    feat:update

--- src/observe/index.js
+++ src/observe/index.js
@@ -26,6 +26,8 @@ export function defineReactive(target, key, value) {
       //修改的时候 会执行set
       console.log("set-data");
       if (value === newValue) return;
+      observe(newValue); //对重新赋值的对象 进行数据劫持
+
       value = newValue;
     },
   });
@@ -36,7 +38,7 @@ export function observe(data) {
   if (typeof data !== "object" || data == null) {
     return; //只对对象进行劫持
   }
-  //如果一个对象被劫持过了，那就不需要再被劫持了（要判断一个对象是否被接吃过，可以增添一个实例，用实例来判断是否被劫持）
+  //如果一个对象被劫持过了，那就不需要再被劫持了（要判断一个对象是否被劫持过，可以增添一个实例，用实例来判断是否被劫持）
 
   //   console.log(data);
   return new Observer(data);
